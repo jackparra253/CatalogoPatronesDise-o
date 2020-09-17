66b74c19078170aeb8c93672bfc553df6365f1e5
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-09-17 06:20:44 -0500

    se agrega sln Gestion Logistica patron de diseño factory method

---
+++ GestionLogistica/GestionLogistica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestionLogistica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
