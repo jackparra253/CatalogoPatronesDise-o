4e76c077f4ae6af849d94a4932578c6f2d4a1ffb
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-09-28 06:04:13 -0500

    add new project design pattern builder

---
+++ AutomotiveIndustry/AutomotiveIndustry.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomotiveIndustry.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
