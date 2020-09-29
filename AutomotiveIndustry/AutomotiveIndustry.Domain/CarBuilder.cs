0d3f9d1ec587994dc9d38e0018cd5328fac166d5
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-09-29 07:02:35 -0500

    add IBuilder and CarBuilder

---
+++ AutomotiveIndustry/AutomotiveIndustry.Domain/CarBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace AutomotiveIndustry.Domain
+{
+    public class CarBuilder : IBuilder
+    {
+        public void GetResult()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetEngine()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetGPS()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetSeats()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetTripComputer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AutomotiveIndustry/AutomotiveIndustry.Domain/IBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace AutomotiveIndustry.Domain
+{
+    public interface IBuilder
+    {
+        void Reset();
+        void SetSeats();
+        void SetEngine();
+        void SetTripComputer();
+        void SetGPS();
+        void GetResult();
+    }
+}
