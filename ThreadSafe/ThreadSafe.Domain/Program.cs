9843e5d8017af3884893065ac31010234742c9dd
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-10-21 06:50:06 -0500

    add main with example singleton

--- ThreadSafe/ThreadSafe.Domain/Program.cs
+++ ThreadSafe/ThreadSafe.Domain/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadSafe.Domain
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:");
+
+            var procces1 = new Thread(() =>
+            {
+                TestSingleton("Hi");
+            });
+
+            var process2 = new Thread(() =>
+            {
+                TestSingleton("Hi singleton!!!");
+            });
+
+            procces1.Start();
+            process2.Start();
+
+            procces1.Join();
+            process2.Join();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            var singleton = Singleton.GetIntance(value);
+            Console.WriteLine(singleton.Value);
         }
     }
 }
