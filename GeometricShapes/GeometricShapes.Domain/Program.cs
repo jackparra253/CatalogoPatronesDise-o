64abef071ad4e5a3b988c12cbeda7d7df30b1c72
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-10-26 06:32:02 -0500

    add new sln dessign pattern adapter

---
+++ GeometricShapes/GeometricShapes.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeometricShapes.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
