2033204db680d510c49f9b9b49487a468ac49d9c
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-11-03 07:03:47 -0500

    add SquarePegAdapter

---
+++ GeometricShapes/GeometricShapes.Domain/SquarePegAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GeometricShapes.Domain;
+
+namespace GeometricShapes.XUnitTest
+{
+    public class SquarePegAdapter
+    {
+        public SquarePeg SquarePeg { get; private set; }
+
+        public SquarePegAdapter(SquarePeg squarePeg)
+        {
+            SquarePeg = squarePeg;
+        }
+
+        public double GetRadius()
+        {
+            return Math.Sqrt(Math.Pow(SquarePeg.Width/ 2, 2) * 2);
+        }
+    }
+}
---
+++ GeometricShapes/GeometricShapes.XUnitTest/SquarePegAdapterUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using GeometricShapes.Domain;
+using Xunit;
+
+namespace GeometricShapes.XUnitTest
+{
+    public class SquarePegAdapterUnitTest
+    {
+        [Fact(DisplayName = "Should GetRadius return radius value")]
+        public void SquarePegAdapter_Case_GetRadius()
+        {
+            var squarePeg = new SquarePeg(9);
+            var squarePegAdapter = new SquarePegAdapter(squarePeg);
+
+            var radius = squarePegAdapter.GetRadius();
+
+            Assert.Equal(5.656854249492381, radius);
+        }
+
+    }
+}
