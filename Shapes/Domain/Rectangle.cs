faa38a937ceb777ca8bd8055b99ae0b3a20feb88
jparra <jparra@dondoctor.com>
2021-01-07 06:58:01 -0500

    add rectangle GetWidth and GetHeight

---
+++ Shapes/Domain/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace Domain
+{
+    public class Rectangle: BaseShape
+    {
+        public Rectangle(int x, int y, int width, int height, Color color):base(x, y, color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public override int GetWidth()
+        {
+            return Width;
+        }
+
+        public override int GetHeight()
+        {
+            return Height;
+        }
+    }
+}
---
+++ Shapes/XUnitTest/RectangleTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class RectangleTest
+    {
+        [Fact(DisplayName = "Should Rectangle GetWith")]
+        public void Rectangle_Case_GetWidth()
+        {
+            int x = 2;
+            int y = 3;
+            int width = 2;
+            int height = 3;
+            Color color = new Color();
+
+            Domain.Rectangle rectangle = new Domain.Rectangle(x, y, width, height, color);
+
+            Assert.Equal(width, rectangle.GetWidth());
+        }
+
+        [Fact(DisplayName = "Should Rectangle GetHeight")]
+        public void Rectangle_Case_GetHeight()
+        {
+            int x = 2;
+            int y = 3;
+            int width = 2;
+            int height = 3;
+            Color color = new Color();
+
+            Domain.Rectangle rectangle = new Domain.Rectangle(x, y, width, height, color);
+
+            Assert.Equal(height, rectangle.GetHeight());
+        }
+
+    }
+}
